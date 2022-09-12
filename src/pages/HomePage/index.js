91074167fe07b1f9c0d7543b46b0fb045f1dfab2
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-09-12 17:49:45 -0300

    criação de inputs e botões

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-    return (
-        <></>
-    )
+  return (
+    <>
+      <h1>WhatsDate</h1>
+
+      <p>
+        Extension created to schedule the time of your messages on your whatsApp
+      </p>
+
+      <Link to="/register">
+        <button>Register</button>
+        <button>Login</button>
+      </Link>
+    </>
+  );
 }
 export default HomePage;
--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -1,6 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+
 function Messages() {
-    return (
-        <></>
-    )
+  const [informations, setInformations] = useState({});
+  //   const [message, setMessage] = useState([]);
+
+  const [form, setForm] = useState({
+    message: "",
+    hours: "",
+    recipient: "",
+  });
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      await axios.post("https://ironrest.herokuapp.com/whatsapp", form);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <>
+      <h1>userName</h1> 
+      <p>
+        Enter your message, choose the time and recipient of your contact list
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <lable>Message</lable>
+        <textarea name="message" value={form.message} onChange={handleChange} />
+
+        <lable>Hours</lable>
+        <input
+          name="hours"
+          type="time"
+          value={form.hours}
+          onChange={handleChange}
+        />
+
+        <lable>Recipient</lable>
+        <input
+          name="recipient"
+          value={form.recipient}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Schedule Message</button>
+      </form>
+    </>
+  );
 }
 export default Messages;
