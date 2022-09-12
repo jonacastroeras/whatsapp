1ab1e06d810a6bca760dfa86f2a900399ed2bd74
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-09-12 15:22:56 -0300

    commit paula

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-import Register from './pages/Register';
-import Messages from './pages/Messages';
-import HomePage from './pages/HomePage';
-
+import "./App.css";
+import Register from "./pages/Register";
+import Messages from "./pages/Messages";
+import HomePage from "./pages/HomePage";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
