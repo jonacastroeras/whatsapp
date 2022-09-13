e93d027a9547816cb0d359ec1eb9a7de32db45a9
AnaPaulaSil <anapaulasilveira52783@gmail.com>
2022-09-13 09:31:25 -0300

    resolving merge

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,17 +1,46 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
+import axios from "axios";
 
 function Register() {
+    const [usersName, setUsersName] = useState([]);
+
+
+    const [form, setForm] = useState({
+        userName: "",
+        password: ""
+      });
+    
+      function handleChange(e) {
+        setForm({ ...form, [e.target.name]: e.target.value });
+      }
+    
+      async function handleSubmit(e) {
+        e.preventDefault();
+    
+        try {
+          await axios.post("https://ironrest.herokuapp.com/whatsapp", form);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+
   return (
     <>
-      {/* qr code */}
+      {/* qr code condicional*/}
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <lable>Username</lable>
-        <input></input>
+        <input name="userName"
+          value={form.userName}
+          onChange={handleChange}/>
 
         <label>Password</label>
-        <input type="password"></input>
+        <input 
+        name="password"
+        type="password"
+        value={form.password}
+        onChange={handleChange}/>
 
         <Link to="/messages">
         <button>Login</button>
